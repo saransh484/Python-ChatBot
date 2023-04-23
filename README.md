6859a0fc94e953cc1749cd905a7702b9220f4714
saransh bhatnagar <saransh8@hotmail.com>
2023-04-24 01:01:09 +0530

    added readme and some minor changes

---
+++ README.md
@@ -0,0 +1,46 @@
+## ChatBot with GUI and BART Summarizer
+
+This project is a ChatBot with a Graphical User Interface (GUI) that uses the BART (Bidirectional and AutoRegressive Transformer) model for summarizing text. The ChatBot can respond to user input in natural language and provide a summary of any text entered by the user.
+
+> note! you need to provide API from huggingface in order to use BART
+
+## Requirements
+
+To run this project, you will need:
+
+- Python 3.x
+- tkinter library
+- nltk library
+- numpy library
+- tensorflow library
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Install the required libraries using pip:
+
+bash
+
+`pip install tkinter pip install nltk pip install numpy pip install tensorflow`
+
+3. Open the `main.py` file and run it using Python.
+
+## Usage
+
+1. Type a message in the text box and press Send button to send it to the ChatBot.
+2. The ChatBot will respond with a message based on its NLP algorithms.
+3. Start your messge with '&' to summerize your large paragraph
+
+## Features
+
+The ChatBot with GUI and BART Summarizer has the following features:
+
+- Graphical User Interface (GUI) for easy user interaction
+- Natural language processing (NLP) for understanding and responding to user input
+- BART summarizer for generating summaries of any text entered by the user
+
+## Credits
+
+This project was developed by me personal project. The BART summarizer was adapted from the Hugging Face Transformers library: https://huggingface.co/transformers/
+
+
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from tkinter import *
+import tkinter as tk
+import tkinter.ttk as ttk
 import chatbot as cb
 import bart as bc
 
 # GUI
-root = Tk()
+root = tk.Tk()
 root.title("Chatbot")
  
-BG_GRAY = "#ABB2B9"
-BG_COLOR = "#17202A"
-TEXT_COLOR = "#EAECEE"
+BG_GRAY = "#44475a"
+BG_COLOR = "#282a36"
+TEXT_COLOR = "#f8f8f2"
  
 FONT = "Arial 14"
 FONT_BOLD = "Arial 13 bold"
@@ -16,30 +17,30 @@ FONT_BOLD = "Arial 13 bold"
 # Send function
 def send():
     send = "You : " + e.get()
-    txt.insert(END, "\n" + send)
+    txt.insert(tk.END, "\n" + send)
  
     user = e.get().lower()
     
     if user[0] == '&':
         ret = bc.summerize(user[1:])
-        txt.insert(END,"\n Bot : " + ret) # type: ignore
+        txt.insert(tk.END,"\n Bot : " + ret) # type: ignore
     else:
         ints = cb.predict_class(user)
         res = cb.get_response(ints, cb.intents)
-        txt.insert(END,"\n Bot : " + res) # type: ignore
+        txt.insert(tk.END,"\n Bot : " + res) # type: ignore
 
-    e.delete(0, END)
+    e.delete(0, tk.END)
 
-lable1 = Label(root,  text="ChatBot", font=FONT_BOLD, pady=10, width=40, height=1).grid(row=0)
+lable1 = tk.Label(root,  text="ChatBot", font=FONT_BOLD, pady=10, width=40, height=1).grid(row=0)
  
-txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+txt = tk.Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
 txt.grid(row=1, column=0, columnspan=2)
  
-scrollbar = Scrollbar(txt)
+scrollbar = ttk.Scrollbar(txt)
 scrollbar.place(relheight=1, relx=0.974)
  
-e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
+e = tk.Entry(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=55)
 e.grid(row=2, column=0)
 
-send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY, command=send).grid(row=2, column=1) # type: ignore
+send = tk.Button(root, text="Send", font=FONT_BOLD, command=send).grid(row=2, column=1) # type: ignore
 root.mainloop()
